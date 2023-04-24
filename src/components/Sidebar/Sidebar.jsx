5839a16300f45b019c71edd20a1ce1a0c5d2c3a5
gift56 <efeasiughu@gmail.com>
2023-04-24 15:19:26 -0700

    now working on the layout of dashboard

--- src/components/Sidebar/Sidebar.jsx
+++ src/components/Sidebar/Sidebar.jsx
@@ -1,7 +1,12 @@
 import React from "react";
+import Navbar from "../Navbar/Navbar";
 
 const Sidebar = () => {
-  return <div className="w-[25%] border-r bg-white  h-screen ">Sidebar</div>;
+  return (
+    <div className="w-[25%] border-r bg-white  h-screen ">
+      <Navbar />
+    </div>
+  );
 };
 
 export default Sidebar;
