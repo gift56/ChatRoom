ceac2dc29fa05424f3a578f99b5bbc583c83a6d2
gift56 <efeasiughu@gmail.com>
2023-04-24 13:58:47 -0700

    working on files

--- src/pages/Register.jsx
+++ src/pages/Register.jsx
@@ -5,11 +5,11 @@ import { Link } from "react-router-dom";
 const Register = () => {
   return (
     <div className="w-full min-h-screen flex flex-col justify-center items-center">
-      <div className="flex items-center justify-center flex-col">
-        <h1>Chat Room</h1>
-        <div className="w-[450px] border flex flex-col px-5 py-4 shadow-smallShadow rounded-md gap-3 lg:h-[450px] lg:overflow-y-auto register bg-white">
+      <div className="flex items-center justify-center flex-col gap-5">
+        <h1 className="text-4xl font-bold">Chat Room</h1>
+        <div className="md:w-[450px] border flex flex-col px-5 py-4 shadow-smallShadow rounded-md gap-3 lg:h-[450px] lg:overflow-y-auto register bg-white">
           <h2 className="text-xl font-bold">Welcome 👋🏻</h2>
-          <p className="text-sm font-normal">
+          <p className="text-sm font-normal text-gray-400">
             Enter your details to create account
           </p>
           <form className="flex flex-col gap-2 items-start justify-start w-full">
@@ -50,9 +50,9 @@ const Register = () => {
               text="Create Account"
               className="mt-4 w-full h-[44px] bg-primary text-white disabled:bg-primary/70"
             />
-            <p className="text-newgray font-normal tex-sm md:text-base text-center w-full mt-2">
+            <p className="text-gray-400 font-normal text-sm md:text-base text-center w-full mt-2">
               Already on our platform?{" "}
-              <Link to="/" className="text-[#3307C5] mx-2">
+              <Link to="/" className="text-[#3307C5] mx-2 font-medium">
                 Sign In
               </Link>
             </p>
