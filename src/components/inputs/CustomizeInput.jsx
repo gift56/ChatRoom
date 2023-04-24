f522d6fc8d0be34d4a31cfb7aed20503fc24f84a
gift56 <efeasiughu@gmail.com>
2023-04-24 11:26:25 -0700

    done working on customize input

--- src/components/inputs/CustomizeInput.jsx
+++ src/components/inputs/CustomizeInput.jsx
@@ -1,9 +1,38 @@
-import React from 'react'
+import React from "react";
 
-const CustomizeInput = () => {
+const CustomizeInput = ({
+  htmlFor,
+  showLabel,
+  id,
+  label,
+  className,
+  labelClassName,
+  inpuClassName,
+  error,
+  onChange,
+  onBlur,
+  accept,
+  containerClass,
+  ...props
+}) => {
   return (
-    <div>CustomizeInput</div>
-  )
-}
+    <div className={`flex flex-col gap-1 w-full ${containerClass}`}>
+      {showLabel === false && (
+        <label htmlFor={htmlFor} className={labelClassName}>
+          {label}
+        </label>
+      )}
+      <input
+        {...props}
+        className={`${className} ${error ? "border !border-red-400" : ""}`}
+        id={id}
+        onBlur={onBlur}
+        onChange={onChange}
+        accept={accept}
+      />
+      {error ? <p className="text-xs text-red-600">{error}</p> : null}
+    </div>
+  );
+};
 
-export default CustomizeInput
+export default CustomizeInput;
