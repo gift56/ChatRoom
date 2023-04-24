27dc3a6fc59d8ec0b78bd7f3c8b7c5b6cde47e3e
gift56 <efeasiughu@gmail.com>
2023-04-24 10:09:11 -0700

    installing dependences

--- src/App.js
+++ src/App.js
@@ -1,9 +1,7 @@
-import React from 'react'
+import React from "react";
 
 const App = () => {
-  return (
-    <div className="text-3xl underline">App</div>
-  )
-}
+  return <div></div>;
+};
 
-export default App
+export default App;
