3536ba74d3b0aa4b70c7a1c64b6702c77f209394
gift56 <efeasiughu@gmail.com>
2023-04-24 14:57:02 -0700

    done creating more components

--- src/components/index.js
+++ src/components/index.js
@@ -1,5 +1,19 @@
 import CustomizeTextarea from "./inputs/CustomizeTextarea";
 import CustomizeInput from "./inputs/CustomizeInput";
 import Button from "./Button/Button";
+import Navbar from "./Navbar/Navbar";
+import Chats from "./Chats/Chats";
+import Chat from "./Chat/Chat";
+import Sidebar from "./Sidebar/Sidebar";
+import Search from "./Search/Search";
 
-export { CustomizeInput, CustomizeTextarea, Button };
+export {
+  CustomizeInput,
+  CustomizeTextarea,
+  Button,
+  Navbar,
+  Sidebar,
+  Search,
+  Chats,
+  Chat,
+};
