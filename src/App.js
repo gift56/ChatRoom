265ce8fb2f96b6c6e8752db03da73cd461d40f62
gift56 <efeasiughu@gmail.com>
2023-04-24 10:19:44 -0700

    working on routing

--- src/App.js
+++ src/App.js
@@ -1,8 +1,13 @@
 import React from "react";
-import { Routes } from "react-router-dom";
+import { Route, Routes } from "react-router-dom";
+import Register from "./pages/Register";
 
 const App = () => {
-  return <Routes>Hello world</Routes>;
+  return (
+    <Routes>
+      <Route path="/" element={<Register />} />
+    </Routes>
+  );
 };
 
 export default App;
