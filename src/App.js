bf7f1723eb15d2c82c86064a7def1c9dbbd90ddb
gift56 <efeasiughu@gmail.com>
2023-04-24 14:27:10 -0700

    working on homepage

--- src/App.js
+++ src/App.js
@@ -2,12 +2,14 @@ import React from "react";
 import { Route, Routes } from "react-router-dom";
 import Register from "./pages/Register";
 import Login from "./pages/Login";
+import Homepage from "./pages/Homepage";
 
 const App = () => {
   return (
     <Routes>
       <Route path="/" element={<Login />} />
       <Route path="/register" element={<Register />} />
+      <Route path="/dashboar" element={<Homepage />} />
     </Routes>
   );
 };
