aacd1eed6401b05e75f14d386044ed5267e5983e
gift56 <efeasiughu@gmail.com>
2023-04-24 15:12:06 -0700

    now working on the layout of dashboard

--- src/pages/Homepage.jsx
+++ src/pages/Homepage.jsx
@@ -1,7 +1,15 @@
 import React from "react";
+import { Chat, Sidebar } from "../components";
 
 const Homepage = () => {
-  return <div>Homepage</div>;
+  return (
+    <div className="w-full flex items-center justify-center h-screen overflow-hidden">
+      <div className="w-full h-full flex items-start overflow-hidden">
+        <Sidebar />
+        <Chat />
+      </div>
+    </div>
+  );
 };
 
 export default Homepage;
