ef4e9c1e9b944c6797636114dfff0df5e569c73e
gift56 <efeasiughu@gmail.com>
2023-04-25 06:36:08 -0700

    stopped here continue tomorrow

--- src/components/Message/Message.jsx
+++ src/components/Message/Message.jsx
@@ -2,18 +2,22 @@ import React from "react";
 
 const Message = () => {
   return (
-    <div className="">
+    <div className="flex gap-5">
       <div>
         <img
           src="https://images.pexels.com/photos/10152592/pexels-photo-10152592.jpeg?auto=compress&cs=tinysrgb&w=600&lazy=load"
           alt="user_image"
           className="w-8 h-8 rounded-full object-cover border"
         />
-        <span>just now</span>
       </div>
       <div>
         <p>Hello my na is john...</p>
-        <img src="https://images.pexels.com/photos/10152592/pexels-photo-10152592.jpeg?auto=compress&cs=tinysrgb&w=600&lazy=load" alt="posted_image" className="w-8 h-8 rounded-full object-cover border" />
+        <img
+          src="https://images.pexels.com/photos/10152592/pexels-photo-10152592.jpeg?auto=compress&cs=tinysrgb&w=600&lazy=load"
+          alt="posted_image"
+          className="object-cover border"
+        />
+        <span>just now</span>
       </div>
     </div>
   );
