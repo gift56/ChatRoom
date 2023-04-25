cde1877a44aaf5de5fd79cb656ab5b217c1c6bbe
gift56 <efeasiughu@gmail.com>
2023-04-25 02:38:18 -0700

    working on chat components

--- src/components/Chat/Chat.jsx
+++ src/components/Chat/Chat.jsx
@@ -5,9 +5,9 @@ import { FiUserPlus } from "react-icons/fi";
 const Chat = () => {
   return (
     <div className="w-[85%]">
-      <div>
-        <h4>Jane Foster</h4>
-        <div></div>
+      <div className="flex w-full items-center justify-between gap-4 bg-slate-300 shadow-sm h-14 px-5">
+        <h4 className="text-xl font-medium">Jane Foster</h4>
+        <div className="flex items-center justify-end gap-4"></div>
       </div>
     </div>
   );
