3e56531a565f6a3f1d48a030508a64c02dcee31d
gift56 <efeasiughu@gmail.com>
2023-04-25 01:40:08 -0700

    now working on the layout of dashboard

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -1,13 +1,20 @@
 import React from "react";
 import Button from "../Button/Button";
+import { VscSignOut } from "react-icons/vsc";
 
 const Navbar = () => {
   return (
     <div className="w-full h-[70px] flex items-center justify-between px-4 sticky top-0">
       <h1 className="text-xl font-bold select-none">Chat Room</h1>
       <div className="flex items-center justify-end gap-2">
-        <div></div>
-        <Button />
+        <div className="flex items-center gap-2">
+          <img src="" alt="" />
+          <span>Gifted world</span>
+        </div>
+        <Button
+          text={<VscSignOut size={20} />}
+          className="text-red-400 cursor-pointer"
+        />
       </div>
     </div>
   );
