96a2e798dd6b113221bba4bda4da1ebd7afbe9f6
gift56 <efeasiughu@gmail.com>
2023-04-25 01:49:43 -0700

    done with navbar

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -8,8 +8,8 @@ const Navbar = () => {
       <h1 className="text-xl font-bold select-none">Chat Room</h1>
       <div className="flex items-center justify-end">
         <div className="flex items-center gap-1">
-          <img src="https://images.pexels.com/photos/10152592/pexels-photo-10152592.jpeg?auto=compress&cs=tinysrgb&w=600&lazy=load" alt="user_image" className="w-6 h-6 rounded-full object-cover border" />
-          <span>Gifted world</span>
+          <img src="https://images.pexels.com/photos/10152592/pexels-photo-10152592.jpeg?auto=compress&cs=tinysrgb&w=600&lazy=load" alt="user_image" className="w-8 h-8 rounded-full object-cover border" />
+          <span>Gift will...</span>
         </div>
         <Button
           text={<VscSignOut size={20} />}
