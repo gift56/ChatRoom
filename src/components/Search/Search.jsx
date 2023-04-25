2f64a2ef4d21fa7d5bb7a4c2025c769469ff344f
gift56 <efeasiughu@gmail.com>
2023-04-25 01:51:00 -0700

    working on search area

--- src/components/Search/Search.jsx
+++ src/components/Search/Search.jsx
@@ -2,7 +2,7 @@ import React from 'react'
 
 const Search = () => {
   return (
-    <div>Search</div>
+    <div className="w-full px-4">Search</div>
   )
 }
 
--- src/components/Sidebar/Sidebar.jsx
+++ src/components/Sidebar/Sidebar.jsx
@@ -1,10 +1,12 @@
 import React from "react";
 import Navbar from "../Navbar/Navbar";
+import Search from "../Search/Search";
 
 const Sidebar = () => {
   return (
     <div className="w-[25%] border-r bg-white  h-screen ">
       <Navbar />
+      <Search />
     </div>
   );
 };
