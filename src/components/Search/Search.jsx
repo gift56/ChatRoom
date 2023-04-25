4ed5d48bf294fed104bdedd67dbff8a6333678de
gift56 <efeasiughu@gmail.com>
2023-04-25 01:58:42 -0700

    working on search area

--- src/components/Search/Search.jsx
+++ src/components/Search/Search.jsx
@@ -16,13 +16,13 @@ const Search = () => {
           className="bg-white border-b h-[40px] w-full focus:border-primary outline-none text-base text-gray-500 placeholder:text-gray-500"
         />
       </div>
-      <div className="flex">
+      <div className="flex items-center justify-start w-full">
         <img
           src="https://images.pexels.com/photos/10152592/pexels-photo-10152592.jpeg?auto=compress&cs=tinysrgb&w=600&lazy=load"
           alt="user_image"
-          className=""
+          className="w-12 h-12 rounded-full object-cover"
         />
-        <h4>Jane Foster</h4>
+        <h4 className="">Jane Foster</h4>
       </div>
     </div>
   );
