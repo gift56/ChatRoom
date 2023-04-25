40e4e2919c17e330e9e4d60f636d5be98a4114e4
gift56 <efeasiughu@gmail.com>
2023-04-25 02:41:43 -0700

    working on chat components

--- src/components/Chat/Chat.jsx
+++ src/components/Chat/Chat.jsx
@@ -7,7 +7,20 @@ const Chat = () => {
     <div className="w-[85%]">
       <div className="flex w-full items-center justify-between gap-4 bg-slate-300 shadow-sm h-14 px-5">
         <h4 className="text-xl font-medium">Jane Foster</h4>
-        <div className="flex items-center justify-end gap-4"></div>
+        <div className="flex items-center justify-end gap-4">
+          <span>
+            <BsCameraVideoFill
+              size={18}
+              className="text-gray-600 cursor-pointer dark:text-gray-300"
+            />
+          </span>
+          <span>
+            <FiUserPlus size={18} className="text-gray-600 cursor-pointer dark:text-gray-300" />
+          </span>
+          <span>
+            <BsThreeDots size={18} className="text-gray-600 cursor-pointer dark:text-gray-300" />
+          </span>
+        </div>
       </div>
     </div>
   );
