58895d1645065a34e99e05d5c0cc4db1fe8aed4f
gift56 <efeasiughu@gmail.com>
2023-04-25 02:33:11 -0700

    working on chat components

--- src/components/Chat/Chat.jsx
+++ src/components/Chat/Chat.jsx
@@ -1,9 +1,16 @@
-import React from 'react'
+import React from "react";
+import { BsCameraVideoFill, BsThreeDots } from "react-icons/bs";
+import { FiUserPlus } from "react-icons/fi";
 
 const Chat = () => {
   return (
-    <div className="w-[85%]">Chat</div>
-  )
-}
+    <div className="w-[85%]">
+      <div>
+        <h4>Jane Foster</h4>
+        <div></div>
+      </div>
+    </div>
+  );
+};
 
-export default Chat
+export default Chat;
