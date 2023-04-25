03d61fdd7c0fc8644b26fbc5822ea09e0ecad966
gift56 <efeasiughu@gmail.com>
2023-04-25 01:57:00 -0700

    working on search area

--- src/components/Search/Search.jsx
+++ src/components/Search/Search.jsx
@@ -16,8 +16,13 @@ const Search = () => {
           className="bg-white border-b h-[40px] w-full focus:border-primary outline-none text-base text-gray-500 placeholder:text-gray-500"
         />
       </div>
-      <div>
-        <img src="https://images.pexels.com/photos/10152592/pexels-photo-10152592.jpeg?auto=compress&cs=tinysrgb&w=600&lazy=load" alt="user_image" />
+      <div className="flex">
+        <img
+          src="https://images.pexels.com/photos/10152592/pexels-photo-10152592.jpeg?auto=compress&cs=tinysrgb&w=600&lazy=load"
+          alt="user_image"
+          className=""
+        />
+        <h4>Jane Foster</h4>
       </div>
     </div>
   );
