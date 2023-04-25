c46c85ccf6816c7c7a28d60046502dde2a82f879
gift56 <efeasiughu@gmail.com>
2023-04-25 01:37:37 -0700

    now working on the layout of dashboard

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -1,11 +1,13 @@
 import React from "react";
+import Button from "../Button/Button";
 
 const Navbar = () => {
   return (
     <div className="w-full h-[70px] flex items-center justify-between px-4 sticky top-0">
-      <h1 className="">Chat Room</h1>
-      <div className="flex items-center justify-start gap-2">
-
+      <h1 className="text-xl font-bold select-none">Chat Room</h1>
+      <div className="flex items-center justify-end gap-2">
+        <div></div>
+        <Button />
       </div>
     </div>
   );
