81653b3f68c85a222211549289a52021fe57c3cc
gift56 <efeasiughu@gmail.com>
2023-04-25 01:30:47 -0700

    now working on the layout of dashboard

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -2,8 +2,11 @@ import React from "react";
 
 const Navbar = () => {
   return (
-    <div className="w-full">
+    <div className="w-full h-[60px] flex items-center justify-between px-4 ">
       <h1>Chat Room</h1>
+      <div className="flex items-center justify-start gap-2">
+
+      </div>
     </div>
   );
 };
