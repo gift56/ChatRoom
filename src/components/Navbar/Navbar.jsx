c73c958adef2d81099355f439b304405f267b246
gift56 <efeasiughu@gmail.com>
2023-04-25 01:11:55 -0700

    now working on the layout of dashboard

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -1,7 +1,11 @@
 import React from "react";
 
 const Navbar = () => {
-  return <div className="w-full">Navbar</div>;
+  return (
+    <div className="w-full">
+      <h1>Chat Room</h1>
+    </div>
+  );
 };
 
 export default Navbar;
