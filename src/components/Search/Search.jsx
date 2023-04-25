50aeebd65143409bc7c975bbfe91c391357d8be2
gift56 <efeasiughu@gmail.com>
2023-04-25 01:52:08 -0700

    working on search area

--- src/components/Search/Search.jsx
+++ src/components/Search/Search.jsx
@@ -1,9 +1,14 @@
-import React from 'react'
+import React from "react";
+import CustomizeInput from "../inputs/CustomizeInput";
 
 const Search = () => {
   return (
-    <div className="w-full px-4">Search</div>
-  )
-}
+    <div className="w-full px-4">
+      <div>
+        <CustomizeInput />
+      </div>
+    </div>
+  );
+};
 
-export default Search
+export default Search;
