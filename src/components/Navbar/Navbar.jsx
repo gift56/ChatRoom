2142a7af24f291b09239f4ea66eefdf9e14e97e6
gift56 <efeasiughu@gmail.com>
2023-04-24 23:59:59 -0700

    now working on the layout of dashboard

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -1,9 +1,7 @@
-import React from 'react'
+import React from "react";
 
 const Navbar = () => {
-  return (
-    <div>Navbar</div>
-  )
-}
+  return <div className="w-full">Navbar</div>;
+};
 
-export default Navbar
+export default Navbar;
