305fd1d66835a0738af73ac495f9747ca5c1ae65
gift56 <efeasiughu@gmail.com>
2023-04-27 08:38:55 -0700

    integrating firebase

--- src/firebase.js
+++ src/firebase.js
@@ -1,9 +1,5 @@
-// Import the functions you need from the SDKs you need
 import { initializeApp } from "firebase/app";
-// TODO: Add SDKs for Firebase products that you want to use
-// https://firebase.google.com/docs/web/setup#available-libraries
 
-// Your web app's Firebase configuration
 const firebaseConfig = {
   apiKey: "AIzaSyBmsrrFmCjjyA3KmRmIx9BEDYgYEi3DDAI",
   authDomain: "chatroom-61dca.firebaseapp.com",
