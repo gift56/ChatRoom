f96b870b090593e832f5f451c30501a5ff9fe499
gift56 <efeasiughu@gmail.com>
2023-04-27 06:06:00 -0700

    working on some stuff

--- src/components/Message/Message.jsx
+++ src/components/Message/Message.jsx
@@ -10,13 +10,15 @@ const Message = () => {
           className="w-8 h-8 rounded-full object-cover border"
         />
       </div>
-      <div className={`flex flex-col`}>
-        <p>Hello my na is john...</p>
+      <div className={`flex flex-col gap-3 relative max-w-[80%]`}>
+        <div>
+        <p className="bg-red-400">Hello my na is john...</p>
         <img
           src="https://images.pexels.com/photos/10152592/pexels-photo-10152592.jpeg?auto=compress&cs=tinysrgb&w=600&lazy=load"
           alt="posted_image"
-          className="object-cover border"
+          className="object-cover border w-20 h-20 rounded-md"
         />
+        </div>
         <span>just now</span>
       </div>
     </div>
