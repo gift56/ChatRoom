45331de630a3c19b2689d7b8950ec993764f1e23
gift56 <efeasiughu@gmail.com>
2023-04-27 06:51:45 -0700

    working on some stuff

--- src/components/Message/Message.jsx
+++ src/components/Message/Message.jsx
@@ -11,18 +11,73 @@ const Message = ({ chat, id }) => {
           className="w-10 h-10 rounded-full object-cover border"
         />
       </div>
-      <div className={`flex flex-col gap-1 relative max-w-[80%]`}>
+      <div className={`flex flex-col gap-1 relative max-w-[50%]`}>
         <div
           className={`w-full bg-red-400 p-4 flex flex-col gap-2  ${
-            owner ? "rounded-tr-xl rounded-bl-xl rounded-br-xl" : "rounded-tl-xl rounded-bl-xl rounded-br-xl"
+            owner
+              ? "rounded-tr-xl rounded-bl-xl rounded-br-xl"
+              : "rounded-tl-xl rounded-bl-xl rounded-br-xl"
           }`}
         >
           <p className="">Hello my na is john...</p>
-          <img
+          <div className="w-full flex items-center gap-1 flex-wrap">
+            <img
+              src="https://images.pexels.com/photos/10152592/pexels-photo-10152592.jpeg?auto=compress&cs=tinysrgb&w=600&lazy=load"
+              alt="posted_image"
+              className="object-cover border w-20 h-20 rounded-md"
+            />{" "}
+            <img
+              src="https://images.pexels.com/photos/10152592/pexels-photo-10152592.jpeg?auto=compress&cs=tinysrgb&w=600&lazy=load"
+              alt="posted_image"
+              className="object-cover border w-20 h-20 rounded-md"
+            />{" "}
+            <img
+              src="https://images.pexels.com/photos/10152592/pexels-photo-10152592.jpeg?auto=compress&cs=tinysrgb&w=600&lazy=load"
+              alt="posted_image"
+              className="object-cover border w-20 h-20 rounded-md"
+            />
+             <img
             src="https://images.pexels.com/photos/10152592/pexels-photo-10152592.jpeg?auto=compress&cs=tinysrgb&w=600&lazy=load"
             alt="posted_image"
             className="object-cover border w-20 h-20 rounded-md"
-          />
+          /> <img
+          src="https://images.pexels.com/photos/10152592/pexels-photo-10152592.jpeg?auto=compress&cs=tinysrgb&w=600&lazy=load"
+          alt="posted_image"
+          className="object-cover border w-20 h-20 rounded-md"
+        /> <img
+        src="https://images.pexels.com/photos/10152592/pexels-photo-10152592.jpeg?auto=compress&cs=tinysrgb&w=600&lazy=load"
+        alt="posted_image"
+        className="object-cover border w-20 h-20 rounded-md"
+      /> <img
+      src="https://images.pexels.com/photos/10152592/pexels-photo-10152592.jpeg?auto=compress&cs=tinysrgb&w=600&lazy=load"
+      alt="posted_image"
+      className="object-cover border w-20 h-20 rounded-md"
+    /> <img
+    src="https://images.pexels.com/photos/10152592/pexels-photo-10152592.jpeg?auto=compress&cs=tinysrgb&w=600&lazy=load"
+    alt="posted_image"
+    className="object-cover border w-20 h-20 rounded-md"
+  /> <img
+  src="https://images.pexels.com/photos/10152592/pexels-photo-10152592.jpeg?auto=compress&cs=tinysrgb&w=600&lazy=load"
+  alt="posted_image"
+  className="object-cover border w-20 h-20 rounded-md"
+/> <img
+            src="https://images.pexels.com/photos/10152592/pexels-photo-10152592.jpeg?auto=compress&cs=tinysrgb&w=600&lazy=load"
+            alt="posted_image"
+            className="object-cover border w-20 h-20 rounded-md"
+          /> <img
+          src="https://images.pexels.com/photos/10152592/pexels-photo-10152592.jpeg?auto=compress&cs=tinysrgb&w=600&lazy=load"
+          alt="posted_image"
+          className="object-cover border w-20 h-20 rounded-md"
+        /> <img
+        src="https://images.pexels.com/photos/10152592/pexels-photo-10152592.jpeg?auto=compress&cs=tinysrgb&w=600&lazy=load"
+        alt="posted_image"
+        className="object-cover border w-20 h-20 rounded-md"
+      /> <img
+      src="https://images.pexels.com/photos/10152592/pexels-photo-10152592.jpeg?auto=compress&cs=tinysrgb&w=600&lazy=load"
+      alt="posted_image"
+      className="object-cover border w-20 h-20 rounded-md"
+    />
+          </div>
         </div>
         <span className="text-xs text-gray-500">just now</span>
       </div>
