90e82f0211a88f426e683f89321ac905b967eb04
gift56 <efeasiughu@gmail.com>
2023-04-28 06:08:27 -0700

    working on register  validation

--- src/pages/Register.jsx
+++ src/pages/Register.jsx
@@ -19,16 +19,43 @@ const Register = () => {
 
   const onSubmit = async (payload, actions) => {
     console.log(payload);
-    // const formData = new FormData();
-    // for (let value in payload) {
-    //   formData.append(value, payload[value]);
-    // }
-    // formData.append("file", payload.file);
     try {
       const res = await createUserWithEmailAndPassword(
         auth,
         payload.email,
         payload.password
+      );
+      const storageRef = ref(storage, payload.full_name);
+
+      const uploadTask = uploadBytesResumable(storageRef, payload.file);
+
+      uploadTask.on(
+        "state_changed",
+        (snapshot) => {
+          // Observe state change events such as progress, pause, and resume
+          // Get task progress, including the number of bytes uploaded and the total number of bytes to be uploaded
+          const progress =
+            (snapshot.bytesTransferred / snapshot.totalBytes) * 100;
+          console.log("Upload is " + progress + "% done");
+          switch (snapshot.state) {
+            case "paused":
+              console.log("Upload is paused");
+              break;
+            case "running":
+              console.log("Upload is running");
+              break;
+          }
+        },
+        (error) => {
+          // Handle unsuccessful uploads
+        },
+        () => {
+          // Handle successful uploads on complete
+          // For instance, get the download URL: https://firebasestorage.googleapis.com/...
+          getDownloadURL(uploadTask.snapshot.ref).then((downloadURL) => {
+            console.log("File available at", downloadURL);
+          });
+        }
       );
     } catch (error) {
       toast.error(error.message, {
