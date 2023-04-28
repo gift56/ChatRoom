8bd3935eca700ffdd5f2f69d9d21d7a747776614
gift56 <efeasiughu@gmail.com>
2023-04-28 11:16:16 -0700

    working on chats right now

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -24,9 +24,9 @@ const Navbar = () => {
   };
   return (
     <div className="w-full h-[70px] flex items-center justify-between px-4 sticky top-0">
-      <h1 className="text-xl font-bold select-none">Chat Space</h1>
+      <h1 className="text-xl font-bold select-none">Chat</h1>
       <div className="flex items-center justify-end">
-        <div className="flex items-center gap-1">
+        <div className="flex items-center gap-1 bg-red-100">
           <img
             src={user?.photoURL}
             alt={user?.displayName}
