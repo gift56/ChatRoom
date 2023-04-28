f980da57ef189290188ca3bb2c1782c5943fae86
gift56 <efeasiughu@gmail.com>
2023-04-28 12:59:19 -0700

    working on chat context

--- src/context/ChatsContext.js
+++ src/context/ChatsContext.js
@@ -5,18 +5,34 @@ import {
   signInWithEmailAndPassword,
   signOut,
 } from "firebase/auth";
+import { UserAuth } from "./AuthContext";
 
 export const ChatContext = createContext();
 
 export const ChatContextProvider = ({ children }) => {
+  const { user } = UserAuth();
   const INITIAL_STATE = {
     chatId: "null",
     user: {},
   };
 
-  const chatReducer=(stae,action)=>{
-    
-  }
+  const chatReducer = (state, action) => {
+    switch (action.type) {
+      case "CHANGE_USER":
+        return {
+          user: action.payload,
+          chatId:
+            user?.uid > searchedUser?.uid
+              ? user?.uid + searchedUser?.uid
+              : searchedUser?.uid + user?.uid,
+        };
+        break;
+
+      default:
+        return state;
+        break;
+    }
+  };
 
   return <ChatContext.Provider value={{}}>{children}</ChatContext.Provider>;
 };
