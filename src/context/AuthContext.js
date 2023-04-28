0b5fb9be90f4bb3af1bc7b3d8cb218172887db7b
gift56 <efeasiughu@gmail.com>
2023-04-28 07:19:33 -0700

    working on auth context

--- src/context/AuthContext.js
+++ src/context/AuthContext.js
@@ -1,3 +1,18 @@
-import { createContext } from "react";
+import { createContext, useContext, useEffect, useState } from "react";
+import { auth } from "../firebase";
+import { onAuthStateChanged } from "firebase/auth";
 
 export const AuthContext = createContext();
+
+export const AuthContextProvider = ({ children }) => {
+  const [user, setUser] = useState({});
+
+  useEffect(() => {
+    const unsubscribe = onAuthStateChanged(auth, (currentUser) => {
+      setUser(currentUser);
+    });
+    return () => {
+      unsubscribe();
+    };
+  });
+};
