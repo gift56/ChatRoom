ec6a378a2f9392f2deead08974cfc929d68d919e
gift56 <efeasiughu@gmail.com>
2023-04-28 06:09:58 -0700

    working on register  validation

--- src/pages/Register.jsx
+++ src/pages/Register.jsx
@@ -30,28 +30,11 @@ const Register = () => {
       const uploadTask = uploadBytesResumable(storageRef, payload.file);
 
       uploadTask.on(
-        "state_changed",
-        (snapshot) => {
-          // Observe state change events such as progress, pause, and resume
-          // Get task progress, including the number of bytes uploaded and the total number of bytes to be uploaded
-          const progress =
-            (snapshot.bytesTransferred / snapshot.totalBytes) * 100;
-          console.log("Upload is " + progress + "% done");
-          switch (snapshot.state) {
-            case "paused":
-              console.log("Upload is paused");
-              break;
-            case "running":
-              console.log("Upload is running");
-              break;
-          }
-        },
         (error) => {
           // Handle unsuccessful uploads
+          console.log(error);
         },
         () => {
-          // Handle successful uploads on complete
-          // For instance, get the download URL: https://firebasestorage.googleapis.com/...
           getDownloadURL(uploadTask.snapshot.ref).then((downloadURL) => {
             console.log("File available at", downloadURL);
           });
