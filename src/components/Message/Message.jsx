a5d8aba5cbbf5bbf9a6466433ba0c2b8676497c1
gift56 <efeasiughu@gmail.com>
2023-04-28 14:39:48 -0700

    adding some scrollign animation

--- src/components/Message/Message.jsx
+++ src/components/Message/Message.jsx
@@ -7,7 +7,11 @@ const Message = ({ chat }) => {
   const { data } = UserChat();
 
   return (
-    <div className={`flex gap-5 ${chat.senderId === user.uid  ? "flex-row" : "flex-row-reverse"}`}>
+    <div
+      className={`flex gap-5 ${
+        chat.senderId === user.uid ? "flex-row-reverse" : "flex-row"
+      }`}
+    >
       <div>
         <img
           src={chat.senderId === user.uid ? user.photoURL : data.user.photoURL}
@@ -15,26 +19,28 @@ const Message = ({ chat }) => {
           className="w-10 h-10 rounded-full object-cover border"
         />
       </div>
-      <div className={`flex flex-col gap-1 relative max-w-[50%]`}>
+      <div className={`flex flex-col gap-1 relative lg:max-w-[50%] max-w-full`}>
         <div
           className={`w-full p-4 flex flex-col gap-2  ${
-            chat.senderId === user.uid 
-              ? "rounded-tr-xl rounded-bl-xl rounded-br-xl bg-white/70"
-              : "rounded-tl-xl rounded-bl-xl rounded-br-xl items-end justify-end bg-primary text-white"
+            chat.senderId === user.uid
+              ? "rounded-tl-xl rounded-bl-xl rounded-br-xl items-end justify-end bg-primary text-white"
+              : "rounded-tr-xl rounded-bl-xl rounded-br-xl bg-white/70"
           }`}
         >
-          <p className="w-full">Hello my na is john...</p>
-          <div className="w-full flex items-center gap-1 flex-wrap">
-            <img
-              src="https://images.pexels.com/photos/10152592/pexels-photo-10152592.jpeg?auto=compress&cs=tinysrgb&w=600&lazy=load"
-              alt="posted_image"
-              className="object-cover border w-20 h-20 rounded-md"
-            />
-          </div>
+          <p className="w-full">{chat.text}</p>
+          {chat.img && (
+            <div className="w-full flex items-center gap-1 flex-wrap">
+              <img
+                src={chat.img}
+                alt={chat.img}
+                className="object-cover border w-80 h-80 rounded-md"
+              />
+            </div>
+          )}
         </div>
         <span
           className={`text-xs text-gray-500 ${
-            chat.senderId === user.uid  ? "text-start" : "text-end"
+            chat.senderId === user.uid ? "text-end" : "text-start"
           } w-full`}
         >
           just now
