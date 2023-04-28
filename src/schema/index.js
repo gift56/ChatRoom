ada0728f7c55781461b4829f855aabc4a65f5e78
gift56 <efeasiughu@gmail.com>
2023-04-28 02:07:27 -0700

    working on register  validation

--- src/schema/index.js
+++ src/schema/index.js
@@ -2,5 +2,5 @@ import * as yup from "yup";
 
 export const registerSchema = yup.object().shape({
   full_name: yup.string().required("Required"),
-  email:yup.string()
+  email: yup.string().email().required("Required"),
 });
