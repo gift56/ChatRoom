212b405f9970252f67b48b77c5fd1a4ebbe76a2f
gift56 <efeasiughu@gmail.com>
2023-04-28 13:31:17 -0700

    working on messages

--- src/components/Messages/Messages.jsx
+++ src/components/Messages/Messages.jsx
@@ -1,10 +1,22 @@
 import React, { useState, useEffect } from "react";
 import Message from "../Message/Message";
 import { UserChat } from "../../context/ChatsContext";
+import { doc, onSnapshot } from "firebase/firestore";
+import { db } from "../../firebase";
 
 const Messages = () => {
   const [messages, setMessages] = useState([]);
   const { data } = UserChat();
+
+useEffect(() => {
+  const unsub=onSnapshot(doc(db,""))
+
+  return () => {
+    
+  }
+}, [])
+
+
 
   return (
     <div className="bg-gray-200 p-4 h-[520px] overflow-y-auto overflow-x-hidden message flex flex-col gap-3">
