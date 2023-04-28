3830ddcfacadea1754378137a003b1c7deed6415
gift56 <efeasiughu@gmail.com>
2023-04-28 12:49:05 -0700

    working on latest chats components

--- src/components/Chats/Chats.jsx
+++ src/components/Chats/Chats.jsx
@@ -8,33 +8,39 @@ const Chats = ({ setOpenChat }) => {
   const { user } = UserAuth();
 
   useEffect(() => {
-    const unsub = onSnapshot(doc(db, "userChats", user.uid), (doc) => {
-      setChats(doc.data());
-    });
-    return () => {
-      unsub();
+    const getChats = () => {
+      const unsub = onSnapshot(doc(db, "userChats", user.uid), (doc) => {
+        setChats(doc.data());
+      });
+      return () => {
+        unsub();
+      };
     };
+
+    user.uid && getChats();
   }, [user.uid]);
 
   return (
     <div className="w-full h-[450px] overflow-y-auto overflow-x-hidden chats">
-      {[0, 1, 2, 3, 4].map((item, i) => (
+      {Object.entries(chats)?.map((chat) => (
         <div
-          key={i}
+          key={chat[0]}
           onClick={() => setOpenChat(true)}
           className="flex items-center justify-start w-full gap-2 hover:bg-gray-200 px-4 p-2 cursor-pointer transition-all duration-300"
         >
           <div className="lg:w-[20%]">
             <img
-              src="https://images.pexels.com/photos/10152592/pexels-photo-10152592.jpeg?auto=compress&cs=tinysrgb&w=600&lazy=load"
-              alt="user_image"
+              src={chat[1].userInfo.photoURL}
+              alt={chat[1].userInfo.displayName}
               className="w-12 h-12 rounded-full object-cover"
             />
           </div>
           <div className="w-[80%] flex flex-col gap-1 justify-start items-start">
-            <h4 className="text-base font-normal">Jane Foster</h4>
+            <h4 className="text-base font-normal">
+              {chat[1].userInfo.displayName}
+            </h4>
             <p className="text-sm text-gray-500">
-              Hello you have a new message...
+              {chat[1].userInfo.lastMessage?.text}
             </p>
           </div>
         </div>
