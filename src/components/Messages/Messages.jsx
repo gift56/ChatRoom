204b45a812097a5bfcfebe5704206c49fb7fd4e4
gift56 <efeasiughu@gmail.com>
2023-04-28 13:32:34 -0700

    working on messages

--- src/components/Messages/Messages.jsx
+++ src/components/Messages/Messages.jsx
@@ -8,15 +8,13 @@ const Messages = () => {
   const [messages, setMessages] = useState([]);
   const { data } = UserChat();
 
-useEffect(() => {
-  const unsub=onSnapshot(doc(db,""))
+  useEffect(() => {
+    const unsub = onSnapshot(doc(db, "chats", data.chatId), () => {
+      doc.exists() && setMessages(doc.data());
+    });
 
-  return () => {
-    
-  }
-}, [])
-
-
+    return () => {};
+  }, []);
 
   return (
     <div className="bg-gray-200 p-4 h-[520px] overflow-y-auto overflow-x-hidden message flex flex-col gap-3">
