8e900bb117b47c6bb338aded798edd8a5f4ac78e
gift56 <efeasiughu@gmail.com>
2023-04-28 13:28:07 -0700

    working on messages

--- src/components/Messages/Messages.jsx
+++ src/components/Messages/Messages.jsx
@@ -1,7 +1,10 @@
-import React from "react";
+import React, { useState } from "react";
 import Message from "../Message/Message";
+import { UserChat } from "../../context/ChatsContext";
 
 const Messages = () => {
+  const [] = useState();
+  const { data } = UserChat();
   return (
     <div className="bg-gray-200 p-4 h-[520px] overflow-y-auto overflow-x-hidden message flex flex-col gap-3">
       {[0, 1, 2].map((item, i) => (
