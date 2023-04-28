2ad8fc9e89ba0f2ea4bfe620083a06b30759c1c4
gift56 <efeasiughu@gmail.com>
2023-04-28 02:11:36 -0700

    working on register  validation

--- src/schema/index.js
+++ src/schema/index.js
@@ -3,4 +3,8 @@ import * as yup from "yup";
 export const registerSchema = yup.object().shape({
   full_name: yup.string().required("Required"),
   email: yup.string().email("Invalid email").required("Required"),
+  password: yup
+    .string()
+    .min(5, "Password must be at least 5 characters long")
+    .required("Required"),
 });
