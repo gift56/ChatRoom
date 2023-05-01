b5099bca47659a999c8826a48ab0b15959ae9c0d
gift56 <efeasiughu@gmail.com>
2023-05-01 05:15:52 -0700

    working on logout modal

--- src/components/Modals/DeleteModal.jsx
+++ src/components/Modals/DeleteModal.jsx
@@ -1,9 +1,7 @@
-import React from 'react'
+import React from "react";
 
-const DeleteModal = () => {
-  return (
-    <div>DeleteModal</div>
-  )
-}
+const DeleteModal = ({ show, setShow }) => {
+  return <div>DeleteModal</div>;
+};
 
-export default DeleteModal
+export default DeleteModal;
