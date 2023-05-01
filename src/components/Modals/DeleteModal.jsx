2e1fcf622520c974868b91347fec86968573a856
gift56 <efeasiughu@gmail.com>
2023-05-01 05:44:50 -0700

    done working on logout modal

--- src/components/Modals/DeleteModal.jsx
+++ src/components/Modals/DeleteModal.jsx
@@ -20,12 +20,12 @@ const DeleteModal = ({ show, setShow, handleLogout }) => {
   return (
     <div
       className={`fixed top-0 right-0 w-full h-full bg-[#00000085] z-40 place-items-center flex justify-center transition-all duration-500 ${
-        show ? "flex" : "flex"
+        show ? "flex" : "hidden"
       }`}
     >
       <motion.div
         ref={modalRef}
-        initial={{ opacity: 0, y: "150px" }}
+        initial={{ opacity: 0, y: "-150px" }}
         whileInView={{ opacity: 1, y: 0 }}
         transition={{ type: "spring", duration: 1 }}
         className="rounded-xl bg-white lg:p-8 p-4 relative w-[90%] md:w-[450px] flex items-start justify-start flex-col gap-6"
@@ -34,7 +34,11 @@ const DeleteModal = ({ show, setShow, handleLogout }) => {
           Are you sure you want to log out!
         </h2>
         <div className="flex items-center justify-start w-full gap-4">
-          <Button text="Cancel" className="border font-semibold text-darkBg" />
+          <Button
+            onClick={() => setShow(false)}
+            text="Cancel"
+            className="border font-semibold text-darkBg"
+          />
           <Button
             onClick={() => handleLogout()}
             text="Logout"
