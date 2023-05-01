6ea29da9c32486cc1da8992e07defb27940edbac
gift56 <efeasiughu@gmail.com>
2023-05-01 05:22:42 -0700

    working on logout modal

--- src/components/Modals/DeleteModal.jsx
+++ src/components/Modals/DeleteModal.jsx
@@ -1,7 +1,29 @@
-import React from "react";
+import { useRef, useEffect } from "react";
 
 const DeleteModal = ({ show, setShow }) => {
-  return <div>DeleteModal</div>;
+  const modalRef = useRef(null);
+
+  useEffect(() => {
+    const handleClickOutside = (event) => {
+      if (modalRef.current && !modalRef.current.contains(event.target)) {
+        setShow(false);
+      }
+    };
+    document.addEventListener("mousedown", handleClickOutside);
+    return () => {
+      document.removeEventListener("mousedown", handleClickOutside);
+    };
+  }, []);
+
+  return (
+    <div
+      className={`fixed top-0 right-0 w-full h-full bg-[#00000085] z-40 place-items-center flex justify-center transition-all duration-500 ${
+        show ? "flex" : "flex"
+      }`}
+    >
+      DeleteModal
+    </div>
+  );
 };
 
 export default DeleteModal;
