7819dc59775b03c1dbb62883609e87dfcd2488b5
gift56 <efeasiughu@gmail.com>
2023-05-01 06:04:00 -0700

    done working on logout modal

--- src/components/Sidebar/Sidebar.jsx
+++ src/components/Sidebar/Sidebar.jsx
@@ -1,11 +1,28 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import Navbar from "../Navbar/Navbar";
 import Search from "../Search/Search";
 import Chats from "../Chats/Chats";
 
 const Sidebar = ({ setOpenChat }) => {
+  const [theme, setTheme] = useState(
+    localStorage.getItem("theme") ? localStorage.getItem("theme") : "system"
+  );
+  
+  const element = document.documentElement;
+  function onWindowMatch() {
+    if (
+      localStorage.theme === "dark" ||
+      (!("theme" in localStorage) &&
+        window.matchMedia("(prefers-color-scheme: dark)").matches)
+    ) {
+      element.classList.add("dark");
+    } else {
+      element.classList.remove("dark");
+    }
+  }
   return (
-    <div className="w-full lg:w-[25%] border-r bg-white h-screen flex items-start">
+    <div className="w-full lg:w-[25%] border-r bg-white h-screen flex items-start justify-start">
+      <div className=""></div>
       <div className="w-full">
         <Navbar />
         <Search setOpenChat={setOpenChat} />
