03b0047a5cbc802b7e841e7277ce7c0459cbff16
gift56 <efeasiughu@gmail.com>
2023-05-01 05:35:13 -0700

    working on logout modal

--- src/components/Modals/DeleteModal.jsx
+++ src/components/Modals/DeleteModal.jsx
@@ -27,7 +27,7 @@ const DeleteModal = ({ show, setShow, handleLogout }) => {
         initial={{ opacity: 0, y: "150px" }}
         whileInView={{ opacity: 1, y: 0 }}
         transition={{ type: "spring", duration: 1 }}
-        className="rounded-xl bg-white  lg:p-8 p-4 relative w-[90%] md:w-[390px]"
+        className="rounded-xl bg-white lg:p-8 p-4 relative w-[90%] md:w-[390px] flex items-start justify-start flex-col gap-6"
       ></motion.div>
     </div>
   );
