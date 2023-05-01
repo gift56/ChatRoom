28782a4d92bbca27668df5b45c56df442576a6c0
gift56 <efeasiughu@gmail.com>
2023-05-01 05:20:19 -0700

    working on logout modal

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useState } from "react";
 import Button from "../Button/Button";
 import { VscSignOut } from "react-icons/vsc";
 import { UserAuth } from "../../context/AuthContext";
@@ -6,10 +6,12 @@ import { useNavigate } from "react-router-dom";
 import { toast } from "react-toastify";
 import logo from "../../assets/chat.json";
 import Lottie from "lottie-react";
+import DeleteModal from "../Modals/DeleteModal";
 
 const Navbar = () => {
   const { logOut, user } = UserAuth();
   const navigate = useNavigate();
+  const [openModal, setOpenModal] = useState(false);
 
   const handleLogout = async () => {
     try {
@@ -26,10 +28,10 @@ const Navbar = () => {
   };
   return (
     <div className="w-full h-[70px] flex items-center justify-between px-4 sticky top-0">
-      <div className="w-[20%] max-w-full sticky top-0">
+      <div className="flex-1 max-w-full sticky top-0">
         <Lottie animationData={logo} loop={true} />
       </div>
-      <div className="flex items-center justify-end">
+      <div className="flex-2 flex items-center justify-end">
         <div className="flex items-center gap-1">
           <img
             src={user?.photoURL}
@@ -44,6 +46,7 @@ const Navbar = () => {
           className="text-red-400 cursor-pointer"
         />
       </div>
+      <DeleteModal show={openModal} setShow={setOpenModal} />
     </div>
   );
 };
