34dfbde5724755fbb4661988bf347f379ea6d904
gift56 <efeasiughu@gmail.com>
2023-05-01 06:05:50 -0700

    done working on logout modal

--- src/components/Sidebar/Sidebar.jsx
+++ src/components/Sidebar/Sidebar.jsx
@@ -2,6 +2,7 @@ import React, { useState, useEffect } from "react";
 import Navbar from "../Navbar/Navbar";
 import Search from "../Search/Search";
 import Chats from "../Chats/Chats";
+import { HiMoon, HiOutlineSun } from "react-icons/hi";
 
 const Sidebar = ({ setOpenChat }) => {
   const [theme, setTheme] = useState(
@@ -38,6 +39,30 @@ const Sidebar = ({ setOpenChat }) => {
         break;
     }
   }, [theme]);
+
+  const renderThemeChangerIcon = () => {
+    if (theme === "dark") {
+      return (
+        <button
+          type="button"
+          className="text-darkColor"
+          onClick={() => setTheme("light")}
+        >
+          <HiOutlineSun fontSize={17} />
+        </button>
+      );
+    } else {
+      return (
+        <button
+          type="button"
+          className="text-darkColor"
+          onClick={() => setTheme("dark")}
+        >
+          <HiMoon fontSize={16} />
+        </button>
+      );
+    }
+  };
   return (
     <div className="w-full lg:w-[25%] border-r bg-white h-screen flex items-start justify-start">
       <div className=""></div>
